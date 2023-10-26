05615e30f0dfda584cf83015e29b1e7ca2f0bc35
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-10-26 16:16:08 +0900

    リスト2-1

---
+++ Java2/Java2_1.java
@@ -0,0 +1,12 @@
+//リスト2-1
+
+public class Java2_1 { 
+    public static void main ( String [] args) {
+        int a;
+        int b;
+        a = 20;
+        b = a + 5;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
