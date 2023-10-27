d2fe26972d377ac2d5b7e811eda2496e976d9ea3
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-10-27 10:57:05 +0900

    リスト2-2　2＿3

---
+++ Java2/Java2_2.java
@@ -0,0 +1,15 @@
+/* リスト2-2（エラーコード）
+
+public class Java2_22_3 {
+    public static void main (String [] args){
+        System.out.println("私の好きな記号は二重引用符 (") です");
+    }
+} */
+
+// リスト2-3 （リスト2-2を正しく表記した場合）
+
+    public class Java2_2 {
+        public static void main (String [] args){
+            System.out.println("私の好きな記号は二重引用符(\")です！");
+        }
+    } 
