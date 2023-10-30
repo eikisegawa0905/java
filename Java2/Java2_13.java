eba181146d1b6f6c1df9ea58ebaf6b3f5b3b54ba
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-10-30 14:29:52 +0900

    リスト２－１３　Sting型をInt型に変換する命令

---
+++ Java2/Java2_13.java
@@ -0,0 +1,10 @@
+// String型をint型に変換する命令
+
+public class Java2_13 {
+    public static void main (String [] args){
+    String age = "32";
+    int n = Integer.parseInt(age);
+        System.out.println("あなたは未来、" + (n+1) + "になりますね" );
+
+    }
+}
