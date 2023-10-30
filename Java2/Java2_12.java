226ee96290582f5cda4d7bc0e8e7311a1b8c12dd
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-10-30 13:40:03 +0900

    リスト２－１２　大きい方の数字を選択させる方法

---
+++ Java2/Java2_12.java
@@ -0,0 +1,11 @@
+ //リスト2-12　大きい方の数字を選択させる命令
+
+ public class Java2_12{
+    public static void main (String [] args){
+    int a = 5 ;
+    int b = 3 ;
+    int m = Math.max(a,b);
+    System.out.println("比較実験" + a + "と" + b + "とで大きい方は・・・" + m);
+
+    }
+}
