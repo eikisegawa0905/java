0e9cc1f2d85aa0b2edfdb3aefabd82fc0aed00f2
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-10-30 11:40:49 +0900

    リスト２－１０　命令実行の文

---
+++ Java2/Java2_10.java
@@ -0,0 +1,10 @@
+ // リスト2-10　命令（画面出力）実行の文
+
+ public class Java2_10{
+    public static void main (String [] args){
+        String name = "すがわら";
+        String message;
+        message = name + "さん、こんにちは";    
+        System.out.println(message);            //命令実行の文　
+    }
+ }
