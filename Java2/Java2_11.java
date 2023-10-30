848991c3fe4c008a7e49a19bee495c38b48c4d4e
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-10-30 13:18:28 +0900

    リスト２－１１　改行なし画面出力の命令

---
+++ Java2/Java2_11.java
@@ -0,0 +1,10 @@
+ //　リスト2-11　改行なし画面出力の命令
+
+public class Java2_11{
+    public static void main ( String [] args){
+    String name = "すがわら";
+    System.out.print ("私の名前は");
+    System.out.print(name);
+    System.out.print("です");
+    }
+}
