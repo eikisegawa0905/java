2d770691cc8fed3d24c62849f02e5d2c1159dc87
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-11-01 19:24:37 +0900

    練習問題３

---
+++ Java3/Kadai3_2.java
@@ -0,0 +1,9 @@
+// 練習問題　3-2　
+
+public class Kadai3_2{
+    public static void main (String [] args){
+        System.out.println("C: age!=30");
+        System.out.println("D: true");
+        System.out.println("E: b + 5 < 20");
+    }
+}
---
+++ Java3/Kadai3_3.java
@@ -0,0 +1,18 @@
+//　練習問題　3-3　の答えtem
+
+public class Kadai3_3 {
+    public static void main(String[] args) {
+        int seibetsu = 0 ;
+        int age = 23 ;
+        System.out.println("こんにちは。");
+        if(seibetsu == 0) {
+            System.out.println("私は男です。");
+        }else{
+            System.out.println("私は女です。");
+        }
+        if (seibetsu == 0 ) {
+            System.out.println(age + "歳です。");
+        }
+            System.out.println("よろしくお願いします。");        
+    }
+}
