3a4ebc885a3f836278f3b0c32ec3e2a2b173c39a
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-11-02 14:43:28 +0900

    リスト4-5　配列の要素に値を代入

---
+++ Java4/Java4_5.java
@@ -0,0 +1,12 @@
+// 配列の要素に値を代入
+
+public class Java4_5 {
+    public static void main(String[] args) {
+        int [] score = new int[5];
+        score[1]= 30;
+        System.out.println(score[1]);
+
+        
+    }
+    
+}
