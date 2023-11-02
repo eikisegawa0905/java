4a8458c8eceffdcb459f93f59bb20cb3249871f4
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-11-02 16:15:20 +0900

    リスト4-6　4-7

---
+++ Java4/Java4_7.java
@@ -0,0 +1,13 @@
+//  リスト4-6　4-7
+
+public class Java4_7 {
+    public static void main(String[] args) {
+        // リスト4-6
+        //  int x ;
+        //System.out.println(x);      ←　xが初期化されてないのでコンパイルエラー
+        
+        //　リスト　4-7
+        int [] score = new int [5];
+        System.out.println(score[0]);
+    }
+}
