72c1927b65a9efb65d1ef9d1811ce57163d55847
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-11-06 14:27:28 +0900

    リスト4－16　親配列と子配列を表示

---
+++ Java4/Java4_16.java
@@ -0,0 +1,10 @@
+//  リスト4-16　親配列と子配列の要素数の表示
+
+public class Java4_16 {
+    public static void main(String[] args) {
+        int [][]scores={{10,20,30},{30,40,50}};
+        System.out.println(scores.length);
+        System.out.println(scores[0].length);
+    }
+
+}
