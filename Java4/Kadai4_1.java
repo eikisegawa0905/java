00dceb0434e30d189c6b8a658928631c00c1c31c
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-11-06 15:51:01 +0900

    練習問題４の回答

---
+++ Java4/Kadai4_1.java
@@ -0,0 +1,12 @@
+// 練習問題4－1　回答
+
+public class Kadai4_1{
+    public static void main(String[] args) {
+        int[]points = new int [4];
+        double[] wights = new double[5];
+        boolean[] answers =new boolean[3];
+        String[] names=new String[3]; 
+
+        
+    }
+}
