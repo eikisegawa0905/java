7a6e05fca8865b9fe1706813e70bc689b9459167
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-11-06 11:33:06 +0900

    ガベージコレクション

---
+++ Java4/Java4_13.java
@@ -0,0 +1,12 @@
+/// ガベージコレクション
+
+public class Java4_13 {
+    public static void main(String[] args) {
+        boolean b =true;
+        if (b == true){
+            int [] i ={1,2,3,};
+        
+        }
+
+    }
+}
