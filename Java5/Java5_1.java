d9f7fefc49f8f9bf9851338488e8945a60f21a4f
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-11-07 10:19:49 +0900

    5-1 メソッドの定義　5-2　メソッドの呼び出し

---
+++ Java5/Java5_1.java
@@ -0,0 +1,22 @@
+//  リスト5-1　シンプルなメソッドの定義 
+
+//public class Java5_1 {
+//    public static void main(String []as) {
+//       System.out.println("こんにちは");
+
+//    }
+//}
+
+//  リスト5-2　メソッドの呼び出し
+
+public class Java5_1{
+    public static void main(String[] args) {
+        System.out.println("メソッドを呼び出します");
+        hello();
+        System.out.println("メソッドの呼び出しが終わりました");
+    }
+    public static void hello(){
+        System.out.println("奏さん、こんにちは");
+    }
+
+}
