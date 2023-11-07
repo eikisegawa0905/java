9b583c3d7ce85af3459457f5be32f221e89bb1da
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-11-07 13:23:12 +0900

    5-6 引数を使わずにできないのか

---
+++ Java5/Java5_6.java
@@ -0,0 +1,14 @@
+//  引数を使わずにできないのか？    エラーコード
+
+public class Java5_6 {
+    public static void main(String[] args) {
+        int X =100;
+        int Y =10;
+
+        add();
+    }
+    public static void add(){
+        // int ans=x + y;          ＜範囲外
+        // System.out.println(x+"+"+y+"="+ans);
+    }
+}
