0a8802b310b7130a326e25415c24b9f4031e7aa4
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-11-09 14:56:30 +0900

    練習問題6-1

---
+++ Java6/commet/Zenhan.java
@@ -0,0 +1,9 @@
+package commet;
+public class Zenhan {
+    public static void doWarusa() {
+        System.out.println("黄な粉でござる。食えませんがの。");
+    }   
+    public static void doTogame() {
+        System.out.println("この老いぼれの目はごまかせんぞ。");
+    }
+}
