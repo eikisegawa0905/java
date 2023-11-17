37e83eb173780b52616aec049ddfe249da4d2ae9
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-11-17 17:04:28 +0900

    継承の基礎　オーバーライド

---
+++ Java11/Java11_4.java
@@ -0,0 +1,11 @@
+package Java11;
+
+public class Java11_4 {
+    public static void main(String[] args) {
+        Hero h=new Hero();
+        h.run();        //<11-6
+        SuperHero sh=new SuperHero();
+        sh.run();
+
+    }
+}
