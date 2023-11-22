829f222dd05b080d1127f48aec06261fd5bdc228
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-11-22 16:22:02 +0900

    リスト13-5

--- Java13/Java13_3.java
+++ Java13/Java13_3.java
@@ -3,9 +3,11 @@ package Java13;
 public class Java13_3 {
     public static void main(String[] args) {
         Wizard w=new Wizard();
+        Character c=w;  //<Character型の箱に代入
         Monster m=new Monster();
-        w.name=("アサカ");
-        w.attack(m);
+        c.name=("アサカ");
+        c.attack(m);
+        //c.fireball(m);  <箱の中身がわからなくっている
         w.fireball(m);
     }
 }    
