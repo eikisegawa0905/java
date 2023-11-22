8d273dcf3034a5b38c8fafd71e87cda7ce56e221
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-11-22 15:50:25 +0900

    リスト13-4

---
+++ Java13/Monster.java
@@ -0,0 +1,15 @@
+package Java13;
+public class Monster {
+    int hp=50;
+    char suffix;
+    public Monster(char suffix){
+        this.suffix=suffix;  
+    }
+    public Monster() {
+    }
+    public void attack(Hero h){
+        System.out.println("モンスター"+this.suffix+"の攻撃");
+        System.out.println("10のダメージ");
+        h.setHp(h.getHp()-10);
+    }
+}
