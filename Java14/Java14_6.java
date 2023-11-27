458f5e577337000a24734120a0440e8f3a16abef
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-11-27 11:20:50 +0900

    暗黙の継承　Objectクラス

---
+++ Java14/Java14_6.java
@@ -0,0 +1,12 @@
+package Java14;
+
+
+
+public class Java14_6 {
+    public static void main(String[] args) {
+        Object o1=new Empty();
+        Object o2=new Java14_4();
+        Object o3="こんにちわ";
+        
+    }
+}
---
+++ Java14/Java14_7.java
@@ -0,0 +1,8 @@
+package Java14;
+
+public class Java14_7 {
+    public void printAnything(Object o){
+        //  何型でもいいから、引数を1つ受け取り画面に表示
+        System.out.println(o.toString());
+    }
+}
