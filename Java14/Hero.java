d3b9f4c1d9aa0c206e3660e72b87218508172c44
eikisegawa0905 <eikisegawa0905@gmail.com>
2023-11-27 11:50:22 +0900

    文字列表現を定義する

--- Java14/Hero.java
+++ Java14/Hero.java
@@ -1,29 +1,11 @@
 package Java14;
-import Java13.*;
-import Java13.Character;
-public class Hero extends Character {
-    public void attack(Monster m){  //<モンスター攻撃用
-        System.out.println(this.name+"の攻撃");
-        System.out.println("敵に10ポイントのダメージを与えた");
-        m.hp-=10;       
-    }
-    public void attack(Goblin g){   //<ゴブリン攻撃用
-        System.out.println(this.name+"の攻撃");
-        System.out.println("敵に10ポイントのダメージを与えた");
-        g.hp-=10;
-    } 
-    public void attack(Slime s){    //<スライム攻撃用
-        System.out.println(this.name+"の攻撃");
-        System.out.println("敵に10ポイントのダメージを与えた");
-        s.hp-=10;
-    }
-    public void attack(DeathBat b){
-        System.out.println();
-    }
-    public int getHp() {
-        return this.hp;
-    }
 
-    public void setHp(int hp) {
+public class Hero {
+    String name;
+    int hp;
+    //  オーバーライド
+    public String toString(Hero h){
+        return"名前:"+this.name+"HP:"+this.hp;
     }
+   
 }
---
+++ Java14/Java14_8.java
@@ -0,0 +1,10 @@
+package Java14;
+
+public class Java14_8 {
+    public static void main(String[] args) {
+        Hero h=new Hero();
+        h.name="ミナト";
+        h.hp=100;
+        System.out.println(h.toString(h));   //引数はhだけでも良い
+    }
+}
